362db60c44e79a9435e24ba78a2ceb4215104c0a
EndrigoRp <146990716+EndrigoRp@users.noreply.github.com>
2024-09-03 22:44:08 -0300

    abstraindo artista de musica

--- src/main/java/com/crud/controller/MusicaController.java
+++ src/main/java/com/crud/controller/MusicaController.java
@@ -6,7 +6,6 @@ import com.crud.domain.service.MusicaService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
